be391b6823fd86edbb902ad284944490f437e709
Ian Whitmore <ianwhitmor@gmail.com>
2021-09-07 16:33:41 -0500

    add state for on submit button so that what user types appears on the page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+
+  state = {
+    q: null,
+  };
+
   handleSearch = event => {
     event.preventDefault();
 
@@ -9,19 +14,26 @@ class App extends React.Component {
     let input = form.elements.search;
     let q = input.value;
     console.log(q);
-  }
+
+    this.setState({ q });
+  };
+
   render() {
     return (
       <div className="App">
           <form onSubmit={this.handleSearch}>
           <label>
-            Search for a location: {' '}
+            Search for a location: 
+            {' '}
             <input type="text" name="search" placeholder="location" />
           </label>
           <div>
             <button type="submit">Explore!</button>
           </div>
         </form>
+        {this.state.q && 
+        <h2>Search: {this.state.q}</h2>
+        }
       </div>
     );
   }
