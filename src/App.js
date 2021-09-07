ceaf64e2e6719cc55cc082c53d9e2166e010b3f9
Ian Whitmore <ianwhitmor@gmail.com>
2021-09-07 16:20:43 -0500

    explore button is now working

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  handleSearch = event => {
+    event.preventDefault();
+
+    let form = event.target;
+    let input = form.elements.search;
+    let q = input.value;
+    console.log(q);
+  }
   render() {
     return (
       <div className="App">
-        <form>
+          <form onSubmit={this.handleSearch}>
           <label>
             Search for a location: {' '}
             <input type="text" name="search" placeholder="location" />
